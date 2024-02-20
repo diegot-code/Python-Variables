c67636b03d368058ed6201dee3ecce30b17d0e4d
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-02-19 20:48:55 -0500

    first commit

---
+++ var.py
@@ -0,0 +1,68 @@
+
+#__________________________________________________________________________________________________
+# Python Variables
+
+variables = "Starting Python Variables..."
+print(variables)
+
+
+x = 15
+# print(x)
+
+y = 7
+# print(y)
+
+z = x
+# print(z)
+
+camelCase = "thisIsAnExample"
+
+PascalCase = "ThisIsAlsoAnExample"
+
+snake_case = "snakes_slither_like_this"
+
+#__________________________________________________________________________________________________
+# Assigning Multiple Values
+
+human1 = name = "Darek"
+# print(human1, "is", name)
+
+#--------------------------------------------------
+
+fruit1, fruit2, fruit3 = "Apple", "Orange", "Grape"
+
+# print(fruit1)
+# print(fruit2)
+# print(fruit3)
+
+#--------------------------------------------------
+
+fruits = ["Durian", "Pineapple", "Watermelon"]
+
+d, p, w = fruits
+
+# print(d)
+# print(p)
+# print(w)
+
+#__________________________________________________________________________________________________
+# Combining Variables
+
+num1 = 40
+num2 = 20
+
+str1 = "Python is"
+str2 = "semi-easy."
+
+# print(num1 + num2)
+# print(str1 + str2)
+
+add = num1 + num2
+# print(add)
+
+# print(num1 - num2)
+# print(str1 - str2) # TypeError
+
+
+
+
